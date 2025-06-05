62b12e6bb2e4766d94ad96d176e892043d951d09
Александр/Alexander <116430779+RedDySter20@users.noreply.github.com>
2025-06-05 23:26:55 +0300

    Update GoogleDriveDataSource.java

--- data/src/main/java/ru/application/data/datasource/GoogleDriveDataSource.java
+++ data/src/main/java/ru/application/data/datasource/GoogleDriveDataSource.java
@@ -7,11 +7,23 @@ import ru.application.domain.entity.Document;
 import java.io.InputStream;
 import java.net.URL;
 public class GoogleDriveDataSource {
+    public static String extractId(String url) {
+        String regex = "/d/([a-zA-Z0-9-_]+)";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(url);
+        if (matcher.find()) {
+            return matcher.group(1);
+        }
+        regex = "/(document|file|spreadsheets|presentation)/d/([a-zA-Z0-9-_]+)";
+        pattern = Pattern.compile(regex);
+        matcher = pattern.matcher(url);
+        if (matcher.find()) {
+            return matcher.group(2);
+        }
+        return null;
+    }
     public Document loadDocument(String googleDriveLink) throws Exception { // TODO: fix links to docx
-        String regex = "/d/([a-zA-Z0-9_-]+)";
-        java.util.regex.Matcher matcher = java.util.regex.Pattern.compile(regex).matcher(googleDriveLink);
-        if (!matcher.find()) throw new IllegalArgumentException("Invalid Google Drive link");
-        String fileId = matcher.group(1);
+        String fileId = extractId(googleDriveLink);
 
         // Construct direct download URL
         String directUrl = "https://drive.google.com/uc?export=download&id=" + fileId;
