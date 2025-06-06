5c69f2a97cb1672b5f6fa696d476af911d1b7179
Fellcast <korivanplay@gmail.com>
2025-06-06 14:46:24 +0300

    feat: пустой TextViewerFragment, начало ветки

---
+++ app/src/main/java/ru/application/speechhint/ui/fragment/TextViewerFragment.java
@@ -0,0 +1,13 @@
+package ru.application.speechhint.ui.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+public class TextViewerFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
